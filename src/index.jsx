58b89477c360f21ef76e921d945d03f9356d328c
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-08-29 17:02:01 +0300

    Add popular navbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
+import Popular from './components/Popular';
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className="light">
+        <div className="container">
+          <Popular />
+        </div>
+      </div>
+    );
   }
 }
 
 const rootElement = document.getElementById('app');
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />, rootElement);
+root.render(<App />);
