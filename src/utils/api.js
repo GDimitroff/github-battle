1a54ff9e93e6a26a90f01e048f657930432aae08
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-08-30 12:16:44 +0300

    API

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,3 +12,70 @@ export async function fetchPopularRepos(language) {
 
   return data.items;
 }
+
+function getErrorMsg(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
+async function getProfile(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const profile = await response.json();
+
+  if (profile.message) {
+    throw new Error(getErrorMsg(profile.message, username));
+  }
+
+  return profile;
+}
+
+async function getRepos(username) {
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos?per_page=100`
+  );
+  const repos = await response.json();
+
+  if (repos.message) {
+    throw new Error(getErrorMsg(repos.message, username));
+  }
+
+  return repos;
+}
+
+function getStarsCount(repos) {
+  return repos.reduce((count, { stargazers_count }) => {
+    return count + stargazers_count;
+  }, 0);
+}
+
+function calculateScore(followers, repos) {
+  return followers * 3 + getStarsCount(repos);
+}
+
+async function getUserData(player) {
+  const [profile, repos] = await Promise.all([
+    getProfile(player),
+    getRepos(player),
+  ]);
+
+  return {
+    profile,
+    score: calculateScore(profile.followers, repos),
+  };
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+export async function battle(players) {
+  const playersData = await Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1]),
+  ]);
+  
+  return sortPlayers(playersData);
+}
