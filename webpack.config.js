6e1f547e3e451812f06f5cd2120dcaa26eda9d73
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-08-31 19:15:16 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    bundle: './src/index.jsx',
-  },
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: 'index_bundle.js',
     publicPath: '/',
   },
   module: {
